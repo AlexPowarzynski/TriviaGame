ba992fff34b5e114766de8e8bdabdabbcbe1ae79
Alex Powarzynski <alexpowarzynski@me.com>
2019-07-25 18:54:36 -0700

    Most functionality is done

--- assets/JavaScript/triviaGame.js
+++ assets/JavaScript/triviaGame.js
@@ -4,68 +4,109 @@ $(document).ready(function () {
     var questions = [
         {
             question: "Which Goalie leads the Devils in Career Wins?",
-            answers: ["Keith Kinkaid", "Martin Brodeur", "Chico Resch", "Sean Burke"],
-            rightanswer: "Martin Brodeur"
+            choices: ["Keith Kinkaid", "Martin Brodeur", "Chico Resch", "Sean Burke"],
+            correctAnswer: "Martin Brodeur"
         },
         {
             question: "Who won the Hart Trophy in 2017/2018?",
-            answers: ["Taylor Hall", "Jesper Bratt", "Will Butcher", "Kyle Palmieri"],
-            rightanswer: "Taylor Hall"
+            choices: ["Taylor Hall", "Jesper Bratt", "Will Butcher", "Kyle Palmieri"],
+            correctAnswer: "Taylor Hall"
         },
         {
             question: "Who scored the series winning goal vs New York in the 2012 playoffs?",
-            answers: ["Zach Parise", "Ilya Kovalchuk", "Adam Henrique", "Petr Sykora"],
-            rightanswer: "Adam Henrique"
+            choices: ["Zach Parise", "Ilya Kovalchuk", "Adam Henrique", "Petr Sykora"],
+            correctAnswer: "Adam Henrique"
         },
         {
             question: "What was the Devils previous arena in New Jersey?",
-            answers: ["Prudential Center", "Continental Airlines Arena", "Floyd Hall Arena", "Mennan Arena"],
-            rightanswer: "Continental Airlines Arena"
+            choices: ["Prudential Center", "Continental Airlines Arena", "Floyd Hall Arena", "Mennan Arena"],
+            correctAnswer: "Continental Airlines Arena"
         },
         {
             question: "What legendary Devil saw the number 26 retired in their honor?",
-            answers: ["Patrik Elias", "Brian Rolston", "Joe Cirella", "Jason Smith"],
-            rightanswer: "Patrik Elias"
+            choices: ["Patrik Elias", "Brian Rolston", "Joe Cirella", "Jason Smith"],
+            correctAnswer: "Patrik Elias"
         },
         {
             question: "Which long time Devils player provides color commentary on MSG+?",
-            answers: ["Stephane Richer", "Bruce Driver", "Ken Daneyko", "John Maclean"],
-            rightanswer: "Ken Daneyko"
+            choices: ["Stephane Richer", "Bruce Driver", "Ken Daneyko", "John Maclean"],
+            correctAnswer: "Ken Daneyko"
         },
         {
             question: "Who did the Devils play in the first regular season game at Prudential Center?",
-            answers: ["Toronto Maple Leafs", "New York Rangers", "Ottawa Senators", "Montreal Canadiens"],
-            rightanswer: "Ottawa Senators"
+            choices: ["Toronto Maple Leafs", "New York Rangers", "Ottawa Senators", "Montreal Canadiens"],
+            correctAnswer: "Ottawa Senators"
         },
         {
             question: "Who is the only player to Captain the Devils to a Stanley Cup?",
-            answers: ["Scott Stevens", "Scott Niedermayer", "John Madden", "Aaron Broten"],
-            rightanswer: "Scott Stevens"
+            choices: ["Scott Stevens", "Scott Niedermayer", "John Madden", "Aaron Broten"],
+            correctAnswer: "Scott Stevens"
         },
         {
             question: "Which Devils rival plays at Madison Square Garden?",
-            answers: ["Philadelphia Flyers", "New York Islanders", "Buffalo Sabres", "New York Rangers"],
-            rightanswer: "New York Rangers"
+            choices: ["Philadelphia Flyers", "New York Islanders", "Buffalo Sabres", "New York Rangers"],
+            correctAnswer: "New York Rangers"
         },
         {
             question: "Who was the Devils first Captain in New Jersey?",
-            answers: ["Steve Tambellini", "Don Lever", "Tapio Levo", "Yvon Vautour"],
-            rightanswer: "Don Lever"
+            choices: ["Steve Tambellini", "Don Lever", "Tapio Levo", "Yvon Vautour"],
+            correctAnswer: "Don Lever"
         }
     ]
 
+    let intervalId;
 
-    var game = {
-        timeleft: 120,
-        correct: 0,
-        incorrect: 0,
+    let timeleft = 120;
+    let correct = 0;
+    let incorrect = 0;
+
+
+    function run() {
+        intervalId = setInterval(decrement, 1000);
+        $(".quiz").prepend("<h2>Time Left: <span id=timeLeft>120</span></h2>");
+        $("#startButton").hide();
+        for (var i = 0; i < questions.length; i++) {
+            quiz.append("<h2>" + questions[i].question + "<h2>");
+            for (var t = 0; t < questions[i].choices.length; t++) {
+                quiz.append("<input type= 'radio' name='question #'" + i + "value='" + questions[i].choices[t] + "'>" + questions[i].choices[t]);
+            }
+        }
+        quiz.append("<button id='done'>Done</button>");
     };
 
-    $("#startButton").on("click", function(){
-        $("#startButton").hide();
-        
+    function showResults() {
+        let userGuess = $("input[type=radio]:checked");
 
+        for (var i = 0; i < userGuess.length; i++) {
+            if (userGuess[i].val() === questions[i].correctAnswer) {
+                correct++;
+            } else {
+                incorrect++;
+            }
+            
+        }
+    }
+    function results(){
+        
+    }
+
+    function decrement() {
+        timeleft--;
+        $("#timeLeft").html(timeleft);
+        if (timeleft === 0) {
+            showResults();
+        }
+    }
+    $("#startButton").on("click", function () {
+        run();
     })
+    $("#done").on("click", function () {
+        showResults();
+    })
+
+
+
+
 
 })
 
